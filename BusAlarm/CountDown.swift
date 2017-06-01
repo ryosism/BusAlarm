197bc98f6accc28a5d1203229a486c15a7219b90
祖父江亮 <sn0wbreeze2000@yahoo.co.jp>
2017-06-01 18:21:44 +0900

    カウントダウンにも対応、レイアウトには要考慮 行きと帰りを入れ替えるボタンを動作するように

--- BusAlarm/CountDown.swift
+++ BusAlarm/CountDown.swift
@@ -14,14 +14,14 @@ class CountDown: UIViewController{
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var timeLabel: UILabel!
     @IBOutlet weak var contDownLabel: UILabel!
+    @IBOutlet weak var 発車まで残り: UILabel!
     
     @IBOutlet weak var nextTrain: UIButton!
 
     @IBOutlet weak var prevTrain: UIButton!
 
     @IBOutlet weak var changeDeperture: UIButton!
-    @IBAction func changeDeperturePushed(_ sender: Any) {
-    }
+
 //    ボタン定義------------------------
     
     let delegate = UIApplication.shared.delegate as! AppDelegate
@@ -43,9 +43,10 @@ class CountDown: UIViewController{
         
         let index:Int = delegate.rowofRidableBusTableNumber(table) + nextPrevCount
         print("index",index)
+        print("table.cont",table.count)
         if index == 0{
             prevTrain.isEnabled = false
-        }else if index == table.count{
+        }else if index == table.count-1{
             nextTrain.isEnabled = false
         }else{
             prevTrain.isEnabled = true
@@ -54,35 +55,61 @@ class CountDown: UIViewController{
 
         let depertureTime:String = table[index]
         print("depertureTime",depertureTime)
-        if delegate.destination == "from_jinryo"{
-            timeLabel.text = "神領発 ,\(depertureTime)発車"
-        }else{
-            timeLabel.text = "中部大学発 ,\(depertureTime)発車"
+        
+        if depertureTime.contains(":") {
+            if delegate.destination == "from_jinryo"{
+                timeLabel.text = "神領発 ,\(depertureTime)発車"
+            }else{
+                timeLabel.text = "中部大学発 ,\(depertureTime)発車"
+            }
+            // ----------------------------------------------------
+            let now = delegate.getnow()
+            //フォーマットの指定
+            let formatter = DateFormatter()
+            formatter.locale = NSLocale(localeIdentifier:"en_US") as Locale!
+            formatter.dateFormat = "HH:mm"
+            formatter.timeZone = NSTimeZone(name:"GMT")! as TimeZone
+            let gettime:NSDate = formatter.date(from: depertureTime)! as NSDate
+            
+            let span:Int = Int(gettime.timeIntervalSince(now as Date))
+            let hour:Int = Int(floor(Double(span/3600)))
+            let minute:Int = (span-hour*3600)/60
+            let second:Int = span-(hour*3600)-(minute*60)
+            
+            print("span",span)
+            発車まで残り.text = "発車まで残り"
+            発車まで残り.font = UIFont.boldSystemFont(ofSize: 25)
+
+            contDownLabel.isHidden = false
+            
+            switch span {
+            case 3600...2560000:
+                contDownLabel.text = "\(hour)時間 \(minute)分 \(second)秒後"
+                contDownLabel.font = UIFont.boldSystemFont(ofSize: 33)
+            case 0...3599:
+                contDownLabel.text = "\(minute)分 \(second)秒後"
+                contDownLabel.font = UIFont.boldSystemFont(ofSize: 40)
+            case -3599 ... -1:
+                contDownLabel.text = "\(-1*minute)分 \(-1*second)秒前"
+                contDownLabel.font = UIFont.boldSystemFont(ofSize: 40)
+                contDownLabel.textColor = UIColor.gray
+            case -2560000 ... -3600:
+                contDownLabel.text = "\(-1*hour)時間 \(-1*minute)分 \(-1*second)秒前"
+                contDownLabel.font = UIFont.boldSystemFont(ofSize: 33)
+                contDownLabel.textColor = UIColor.gray
+            default:
+                break
+            }
+        }else{ //約3~5分間隔で運行の表示なら
+            if delegate.destination == "from_jinryo"{
+                timeLabel.text = "神領発"
+            }else{
+                timeLabel.text = "中部大学発"
+            }
+            発車まで残り.text = depertureTime
+            発車まで残り.font = UIFont.boldSystemFont(ofSize: 30)
+            contDownLabel.isHidden = true
         }
-// ----------------------------------------------------
-        let now = delegate.getnow()
-        //フォーマットの指定
-        let formatter = DateFormatter()
-        formatter.locale = NSLocale(localeIdentifier:"en_US") as Locale!
-        formatter.dateFormat = "HH:mm"
-        formatter.timeZone = NSTimeZone(name:"GMT")! as TimeZone
-        let gettime:NSDate = formatter.date(from: depertureTime)! as NSDate
-        
-        let span:Int = Int(gettime.timeIntervalSince(now as Date))
-        let hour:Int = Int(floor(Double(span/3600)))
-        let minute:Int = (span-hour*3600)/60
-        let second:Int = span-(hour*3600)-(minute*60)
-        
-        print(span)
-        
-        if span >= 3600{
-            contDownLabel.text = "\(hour)時間 \(minute)分 \(second)秒"
-            contDownLabel.font = UIFont.boldSystemFont(ofSize: 22)
-        }else{
-            contDownLabel.text = "\(minute)分 \(second)秒"
-            contDownLabel.font = UIFont.boldSystemFont(ofSize: 37)
-        }
-// ----------------------------------------------------
     }
     
     override func viewDidLoad() {
@@ -104,6 +131,17 @@ class CountDown: UIViewController{
         nextPrevCount -= 1
         viewWillAppear(true)
     }
+    
+    @IBAction func changeDeperturePushed(_ sender: Any) {
+        nextPrevCount = 0
+        if delegate.destination == "from_jinryo"{
+            delegate.destination = "from_school"
+        }else{
+            delegate.destination = "from_jinryo"
+        }
+        viewWillAppear(true)
+    }
+
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
