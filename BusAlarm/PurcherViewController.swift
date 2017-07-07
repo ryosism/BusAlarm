5ab478c26c03b00cba9f5042f5040fcf49ddf85c
ryosism <sn0wbreeze2000@yahoo.co.jp>
2017-07-08 01:15:18 +0900

    必要のないコードを削除

--- BusAlarm/PurcherViewController.swift
+++ BusAlarm/PurcherViewController.swift
@@ -12,15 +12,9 @@ import SwiftGifOrigin
 class PurcherViewController: UIViewController {
     
     @IBOutlet weak var previewGifView: UIImageView!
-    
-    override func viewWillAppear(_ animated: Bool) {
-       
-    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-//        let gifurl = URL(string: Bundle.main.path(forResource: "previewgif", ofType: "gif")!)
-//        let req = URLRequest(url:gifurl!)
 
         let gifImage = UIImage.gif(name: "previewgif")
         previewGifView.image = gifImage
