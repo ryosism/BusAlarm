8fb3597e4db11024dd3451ac94216ecdd94ba78b
ryosism <sn0wbreeze2000@yahoo.co.jp>
2017-07-08 16:12:55 +0900

    購入ボタン完成

--- BusAlarm/PurcherAction.swift
+++ BusAlarm/PurcherAction.swift
@@ -16,6 +16,14 @@ class PurcherAction: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        yen240.layer.borderWidth = 2.0 // 枠線の幅
+        yen240.layer.borderColor = UIColor.init(colorLiteralRed: 0.1, green: 0.6, blue: 1.0, alpha: 1).cgColor // 枠線の色
+        yen240.layer.cornerRadius = 10.0 // 角丸のサイズ
+        
+        restoreButton.layer.borderWidth = 2.0 // 枠線の幅
+        restoreButton.layer.borderColor = UIColor.init(colorLiteralRed: 0.1, green: 0.6, blue: 1.0, alpha: 1).cgColor // 枠線の色
+        restoreButton.layer.cornerRadius = 10.0 // 角丸のサイズ
 
     }
     
